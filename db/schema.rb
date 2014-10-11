7c6e6bbebac61cbfc86bbe1c89457f2f1e691850
funnythingz <funnythingz69+github@gmail.com>
2014-10-11 19:03:56 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011084318) do
+ActiveRecord::Schema.define(version: 20141011100143) do
 
   create_table "contacts", force: true do |t|
     t.string   "firstname",  limit: 255
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20141011084318) do
     t.string   "phone",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "contact_id", limit: 4
   end
+
+  add_index "phones", ["contact_id"], name: "index_phones_on_contact_id", using: :btree
 
 end
