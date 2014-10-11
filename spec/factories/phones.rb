b1d00f6070fcc5bb3debcc41965e09569969b5fe
funnythingz <funnythingz69+github@gmail.com>
2014-10-11 22:40:26 +0900

    refactor use FactoryGirl for models/Phone

--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -1,8 +1,22 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require 'faker'
+
 FactoryGirl.define do
   factory :phone do
-    phone_type "MyString"
-    phone "MyString"
+    association :contact
+    phone Faker::PhoneNumber.phone_number
+
+    factory :home_phone do
+      phone_type 'home'
+    end
+
+    factory :work_phone do
+      phone_type 'work'
+    end
+
+    factory :modile_phone do
+      phone_type 'modile'
+    end
   end
 end
