023292af49894b3d8f8d1e90de2bcb89021ff7c8
funnythingz <funnythingz69+github@gmail.com>
2014-10-11 21:49:32 +0900

    refactor use FactoryGirl

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -7,33 +7,26 @@ describe 'Contact' do
     expect(FactoryGirl.build(:contact)).to be_valid
   end
 
-  # 姓と名とメールがあれば有効な状態であること
-  it 'is valid with a firstname, lastname and email' do
-    contact = Contact.new(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: Faker::Internet.email)
-
-    expect(contact).to be_valid
-  end
-
   # 名がなければ無効な状態であること(名: 必須)
   it 'is invalid without a firstname' do
-    expect(Contact.new).to have(1).errors_on(:firstname)
+    expect(FactoryGirl.build(:contact, firstname: nil)).to have(1).errors_on(:firstname)
   end
 
   # 姓がなければ無効な状態であること(姓: 必須)
   it 'is invalid without a lastname' do
-    expect(Contact.new).to have(1).errors_on(:lastname)
+    expect(FactoryGirl.build(:contact, lastname: nil)).to have(1).errors_on(:lastname)
   end
 
   # メールアドレスがなければ無効な状態であること(メールアドレス: 必須)
   it 'is invalid without an email address' do
-    expect(Contact.new).to have(1).errors_on(:email)
+    expect(FactoryGirl.build(:contact, email: nil)).to have(1).errors_on(:email)
   end
 
   # 重複したメールアドレスなら無効な状態であること
   it 'is invalid with a duplicate email address' do
     email = Faker::Internet.email
-    Contact.create(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: email)
-    contact = Contact.new(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: email)
+    FactoryGirl.create(:context, email: email)
+    contact = FactoryGirl.build(:contact, email: email)
 
     expect(contact).to have(1).errors_on(:email)
   end
@@ -42,10 +35,9 @@ describe 'Contact' do
   it "returns a contact's full name as a string" do
     first_name = Faker::Name.first_name
     last_name = Faker::Name.last_name
-    fullname = "#{first_name} #{last_name}"
-    contact = Contact.new(firstname: first_name, lastname: last_name, email: Faker::Internet.email)
+    contact = FactoryGirl.build(:contact, lastname: last_name, firstname: first_name)
 
-    expect(contact.fullname).to eq fullname
+    expect(contact.fullname).to eq "#{first_name} #{last_name}"
   end
 
   # 文字で姓をフィルタする
