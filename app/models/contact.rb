09200cd0f3330b73c847fcb6188526a1b40203c9
funnythingz <funnythingz69+github@gmail.com>
2014-10-11 18:38:57 +0900

    DRY models/contact_spec

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true, uniqueness: true
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -9,18 +9,23 @@ describe 'Contact' do
     expect(contact).to be_valid
   end
 
-  # 名がなければ無効な状態であること
-  it 'is invalid withoud a firstname' do
+  # 名がなければ無効な状態であること(名: 必須)
+  it 'is invalid without a firstname' do
     expect(Contact.new).to have(1).errors_on(:firstname)
   end
 
-  # 姓がなければ無効な状態であること
-  it 'is invalid withoud a lastname' do
+  # 姓がなければ無効な状態であること(姓: 必須)
+  it 'is invalid without a lastname' do
     expect(Contact.new).to have(1).errors_on(:lastname)
   end
 
-  # メールアドレスがなければ無効な状態であること
-  it 'is invalid withoud an email address' do
+  # メールアドレスがなければ無効な状態であること(メールアドレス: 必須)
+  it 'is invalid without an email address' do
+    expect(Contact.new).to have(1).errors_on(:email)
+  end
+
+  # 重複したメールアドレスなら無効な状態であること
+  it 'is invalid with a duplicate email address' do
     email = Faker::Internet.email
     Contact.create(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: email)
     contact = Contact.new(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name, email: email)
@@ -28,12 +33,14 @@ describe 'Contact' do
     expect(contact).to have(1).errors_on(:email)
   end
 
-  # 重複したメールアドレスなら無効な状態であること
-  it 'is invalid with a duplicate email address' do
-  end
-
   # 連絡先のフルネームを文字列として返すこと
   it "returns a contact's full name as a string" do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    fullname = "#{first_name} #{last_name}"
+    contact = Contact.new(firstname: first_name, lastname: last_name, email: Faker::Internet.email)
+
+    expect("#{contact.firstname} #{contact.lastname}").to eq fullname
   end
 
 end
