0fd4ecce6fa58647cd908003547a81d915938aae
funnythingz <funnythingz69+github@gmail.com>
2014-10-11 21:33:21 +0900

    modified use Faker for FactoryGirl

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,9 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require 'faker'
+
 FactoryGirl.define do
   factory :contact do
-    firstname "MyString"
-    lastname "MyString"
-    email "MyString"
+    firstname Faker::Name.first_name
+    lastname Faker::Name.last_name
+    email Faker::Internet.email
   end
 end
