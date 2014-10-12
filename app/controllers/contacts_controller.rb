526ce1a3681488d366dcd82164b14588904695e1
funnythingz <funnythingz69+github@gmail.com>
2014-10-12 20:58:19 +0900

    not yet contact_controller_spec fixed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,84 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
+  # GET /contacts
   def index
-    @contacts = Contact.all
+    if params[:letter]
+      @contacts = Contact.by_letter(params[:letter])
+    else
+      @contacts = Contact.all
+    end
 
     respond_to do |format|
       format.html
       format.csv do
-        send_data Contact.to_csv(@contacts), type: 'text/csv; charset=iso-8859-1, header=present', disposition: 'attachment; filename=contacts.csv'
+        send_data @contacts.to_csv, type: 'text/csv; charset=iso-8859-1, header=present', disposition: 'attachment; filename=contacts.csv'
+      end
+      format.json
+    end
+  end
+
+  # GET /contacts/1
+  def show
+  end
+
+  # GET /contacts/new
+  def new
+    @contact = Contact.new
+  end
+
+  # GET /contacts/1/edit
+  def edit
+  end
+
+  # POST /contacts
+  def create
+    @contact = Contact.new(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @contact }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
       end
     end
   end
+
+  # PATCH /contacts/1
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.html { redirect_to @contact, notice: 'Contact was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /contacts/1
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.html { redirect_to contacts_url }
+      format.json { head :no_content }
+    end
+  end
+
+  def hide_contact
+    redirect_to contacts_url
+  end
+
+  private
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+
+    def contact_params
+      params.require(:contact).permit(:firstname, :lastname, :email,
+                                      :phones_attributes => [:id, :phone, :phone_type])
+    end
 end
