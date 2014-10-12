fcbaaba5f7cf05acc0ac477491e71314b31763dc
funnythingz <funnythingz69+github@gmail.com>
2014-10-12 12:34:44 +0900

    added 'GET' test of contact_controller

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -9,33 +9,93 @@ describe ContactsController do
       # すべての連絡先を配列にまとめること
       it 'populates an array of all contacts'
 
+      # 指定された文字で始まる連絡先を配列にまとめること
+      it 'populates an array of contacts starting with the letter' do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index, letter: 'S'
+
+        expect(assigns(:contacts)).to match_array([smith])
+      end
+
       # index テンプレートを表示すること
-      it 'renders the :index template'
+      it 'renders the :index template' do
+        get :index, letter: 'S'
+
+        expect(response).to render_template :index
+      end
+    end
+
+    # params[:letter] がない場合
+    context 'without params[:letter]' do
+      # 全ての連絡先を配列にまとめること
+      it 'populates an array of all contacts' do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index
+
+        expect(assigns(:contacts)).to match_array([smith, jones])
+      end
+
+      # index テンプレートを表示すること
+      it 'renders the :index template' do
+        get :index
+
+        expect(response).to render_template :index
+      end
     end
   end
 
   describe 'GET #show' do
+
+    before :each do
+      @contact = create(:contact)
+      get :show, id: @contact
+    end
+
     # @contact に要求された連絡先を割り当てること
-    it 'assigns the requested contact to @contact'
+    it 'assigns the requested contact to @contact' do
+      expect(assigns(:contact)).to eq @contact
+    end
 
     # :show テンプレートを表示すること
-    it 'renders the :show template'
+    it 'renders the :show template' do
+      expect(response).to render_template :show
+    end
   end
 
   describe 'GET #new' do
+
+    before :each do
+      get :new
+    end
     # @contact に新しい連絡先を割り当てること
-    it 'assigns a new Contact to @contact'
+    it 'assigns a new Contact to @contact' do
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
 
     # :new テンプレートを表示すること
-    it 'renders the :new template'
+    it 'renders the :new template' do
+      expect(response).to render_template :new
+    end
   end
 
   describe 'GET #edit' do
+
+    before :each do
+      @contact = create(:contact)
+      get :show, id: @contact
+    end
+
     # @contact に要求された連絡先を割り当てること
-    it 'assigns the requested contact to @contact'
+    it 'assigns the requested contact to @contact' do
+      expect(assigns(:contact)).to eq @contact
+    end
 
     # :edit テンプレートを表示すること
-    it 'renders the :edit template'
+    it 'renders the :edit template' do
+      expect(response).to render_template :edit
+    end
   end
 
   describe 'POST #create' do
