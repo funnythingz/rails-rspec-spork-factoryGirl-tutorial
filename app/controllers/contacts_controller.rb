f2eb29b622ee0def01e52c34be7327f2c3968790
funnythingz <funnythingz69+github@gmail.com>
2014-10-12 15:40:12 +0900

    added contacts.csv

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,14 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @contacts = Contact.all
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        send_data Contact.to_csv(@contacts), type: 'text/csv; charset=iso-8859-1, header=present', disposition: 'attachment; filename=contacts.csv'
+      end
+    end
+  end
 end
